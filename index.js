494a7b2b60982aee584700d8b0ba6d249ddce243
Pervez <pervezhossain1998@gmail.com>
2022-10-25 00:32:02 +0600

    cors setup done

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express()
 const port = 5000
+const cors = require('cors')
+
+app.use(cors())
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
