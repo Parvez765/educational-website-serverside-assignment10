3ca009917d4dbc2b3b7e9af57e7290c8af0efb68
Pervez <pervezhossain1998@gmail.com>
2022-10-25 13:46:58 +0600

    vercel file added

--- index.js
+++ index.js
@@ -2,8 +2,32 @@ const express = require("express")
 const app = express()
 const port = 5000
 const cors = require('cors')
+const category = require("./category.json")
+const courses = require("./courses.json")
 
 app.use(cors())
+
+
+app.get("/category/:id", (req, res) => {
+    res.send(category)
+})
+
+app.get("/courses", (req, res) => {
+    res.send(courses)
+})
+
+app.get("/courses/:id", (req, res) => {
+    const id = req.params.id
+    const selectedCourse = courses.find(cource => cource.id == id)
+    res.send(selectedCourse)
+})
+
+app.get("/courses/category/:id", (req, res) => {
+    const id = req.params.id
+    const selectedCourse = courses.filter(cource => cource.category == id)
+    res.send(selectedCourse)
+})
+
 
 
 app.get('/', (req, res) => {
